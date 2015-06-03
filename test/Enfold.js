09a5c988fae688cc2a48cc2172bc630acf174fec
Conrad Zimmerman <me@conradz.com>
2015-06-03 11:00:12 -0400

    Add invalidate method

--- test/Enfold.js
+++ test/Enfold.js
@@ -4,6 +4,8 @@ var path = require('path')
 var vm = require('vm')
 var test = require('tap').test
 
+var fixtures = path.join(__dirname, 'fixtures')
+
 test('single entry', function (t) {
   var parse = mockParse()
   var entry = path.join(__dirname, 'entry.js')
@@ -15,6 +17,81 @@ test('single entry', function (t) {
   function done (err, result) {
     t.error(err)
     t.equal(result, 'test')
+    t.end()
+  }
+})
+
+test('invalidate file', function (t) {
+  var parse = mockParse()
+  var entry = path.join(__dirname, 'entry.js')
+  parse.set(entry, 'console.log("foo")')
+
+  var bundle = new Enfold({ parse: parse })
+  runPack([entry], bundle, first)
+
+  function first (err, result) {
+    t.error(err)
+    t.equal(result, 'foo')
+
+    parse.set(entry, 'console.log("bar")')
+    bundle.invalidate(entry)
+    runPack([entry], bundle, second)
+  }
+
+  function second (err, result) {
+    t.error(err)
+    t.equal(result, 'bar')
+    t.end()
+  }
+})
+
+test('invalidate with different dependencies', function (t) {
+  var parse = mockParse()
+  var entry = path.join(fixtures, 'entry.js')
+  parse.set(entry, 'console.log(require("./foo"))')
+  parse.set(path.join(fixtures, 'foo.js'), 'module.exports = "foo"')
+  parse.set(path.join(fixtures, 'bar.js'), 'module.exports = "bar"')
+
+  var bundle = new Enfold({ parse: parse })
+  runPack([entry], bundle, first)
+
+  function first (err, result) {
+    t.error(err)
+    t.equal(result, 'foo')
+
+    parse.set(entry, 'console.log(require("./bar"))')
+    bundle.invalidate(entry)
+    runPack([entry], bundle, second)
+  }
+
+  function second (err, result) {
+    t.error(err)
+    t.equal(result, 'bar')
+    t.end()
+  }
+})
+
+test('invalid non-entry dependency', function (t) {
+  var parse = mockParse()
+  var entry = path.join(fixtures, 'entry.js')
+  var a = path.join(fixtures, 'a.js')
+  parse.set(entry, 'console.log(require("./a"))')
+  parse.set(a, 'module.exports = "a"')
+
+  var bundle = new Enfold({ parse: parse })
+  runPack([entry], bundle, first)
+
+  function first (err, result) {
+    t.error(err)
+    t.equal(result, 'a')
+    parse.set(a, 'module.exports = "b"')
+    bundle.invalidate(a)
+    runPack([entry], bundle, second)
+  }
+
+  function second (err, result) {
+    t.error(err)
+    t.equal(result, 'b')
     t.end()
   }
 })
