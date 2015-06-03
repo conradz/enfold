67c26161e56aa97df504b0a3548c7a7c05c1906e
Conrad Zimmerman <me@conradz.com>
2015-06-03 10:18:05 -0400

    Add pack method to Enfold class

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-var escodegen = require('escodegen')
-var walk = require('./lib/walk')
-var link = require('./lib/link')
-var pack = require('./lib/pack')
 var Enfold = require('./lib/Enfold')
 
 module.exports = enfold
 
 function enfold (entry, cb) {
   var bundle = new Enfold()
-  return walk(entry, bundle, walked)
-
-  function walked (err, graph) {
-    if (err) {
-      return cb(err)
-    }
-
-    graph = link(graph)
-    var result = pack(graph)
-    var code = escodegen.generate(result)
-    return cb(null, code)
-  }
+  bundle.pack(entry, cb)
 }
