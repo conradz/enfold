1a9fda1ea6b2a431de047c2f08dfe999ea31e2b4
Conrad Zimmerman <me@conradz.com>
2015-06-02 21:31:54 -0400

    Move file reading to parse

--- test/parse.js
+++ test/parse.js
@@ -1,10 +1,16 @@
 var parse = require('../lib/parse')
+var path = require('path')
 var test = require('tap').test
 
 test('parse simple program', function (t) {
-  var ast = parse('a = 1', 'a.js')
-  t.equal(ast.type, 'Program')
-  t.equal(ast.loc.source, 'a.js')
-  t.equal(ast.body.length, 1)
-  t.end()
+  var file = path.join(__dirname, 'fixtures', 'parse.js')
+  parse(file, done)
+
+  function done (err, ast) {
+    t.error(err)
+    t.equal(ast.type, 'Program')
+    t.equal(ast.loc.source, file)
+    t.equal(ast.body.length, 1)
+    t.end()
+  }
 })
